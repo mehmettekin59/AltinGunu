c8eab1b1dd9daee92238e3783c3156aa3654669e
mehmettekin59 <mehmettekin59@gmail.com>
2025-04-19 15:58:31 +0300

    cark sorunları çözüldü, API_UPDATE_INTERVAL and LANGUAGE_CODE calisiyor.

--- app/src/main/java/com/mehmettekin/altingunu/presentation/screens/enter/EnterScreen.kt
+++ app/src/main/java/com/mehmettekin/altingunu/presentation/screens/enter/EnterScreen.kt
@@ -2,7 +2,6 @@ package com.mehmettekin.altingunu.presentation.screens.enter
 
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.ExperimentalFoundationApi
-import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.layout.Arrangement
@@ -14,7 +13,6 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowForward
-import androidx.compose.material.icons.automirrored.filled.List
 import androidx.compose.material.icons.filled.*
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
@@ -49,7 +47,6 @@ import java.text.DecimalFormatSymbols
 import java.util.Locale
 import kotlin.math.absoluteValue
 import com.mehmettekin.altingunu.R
-import kotlinx.coroutines.delay
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
