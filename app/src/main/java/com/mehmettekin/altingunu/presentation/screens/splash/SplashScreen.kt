7fb7223125055f9adea02c02de3562e0ab1fc48e
mehmettekin59 <mehmettekin59@gmail.com>
2025-04-29 21:58:51 +0300

    sss

--- app/src/main/java/com/mehmettekin/altingunu/presentation/screens/splash/SplashScreen.kt
+++ app/src/main/java/com/mehmettekin/altingunu/presentation/screens/splash/SplashScreen.kt
@@ -13,7 +13,6 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.ChevronRight
@@ -29,8 +28,6 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.alpha
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
